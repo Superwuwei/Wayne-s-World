6e4be84c660951c384e0b1200faef439960e7c60
WayneGameDev <wuwei@dipipa.com>
2025-11-13 10:15:22 +0800

    Initial commit

---
+++ src/main/java/waynegamedev/waynesworld/WaynesWorldClient.java
@@ -0,0 +1,11 @@
+package waynegamedev.waynesworld;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WaynesWorldClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
